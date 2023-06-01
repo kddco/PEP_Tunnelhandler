88966fab7e729e3d781fd0a17cfbca150311b34f
kddco <52121443+kddco@users.noreply.github.com>
2023-06-01 21:05:28 +0800

    改用hashmap+setInterval檢查時效
    捨棄session

--- pep.js
+++ pep.js
@@ -1,48 +1,38 @@
 const express = require('express');
-const session = require('express-session');
 const app = express();
 
-var challenge = require('./challenge_fun.js');
-const host = '127.0.0.1';
-const port = process.env.PORT || 3344;
-
-
-app.use(session({
-    secret: challenge.get_128bits(), // 需要設定一個秘鑰
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 60000, // 設定 session 過期時間，此處設定為 1 分鐘
-    }
-}));
+const globalMap = new Map(); // 這是我們的全域 HashMap
+const expirationTime = 60000; // 過期時間設定為一分鐘
+const intervalTime = 30000; // 檢查間隔時間設定為三十秒
 
 app.get('/internaltoken', (req, res) => {
-  console.log(req.query);
-    if(req.session.name && req.session.displayName) {
-        // 如果 session 存在，則更新 token
-        console.log("session update\n","name",req.session.name,"\n","displayName",req.session.displayName);
-        req.session.touch();
-        res.send('更新session: displayName' + req.session.displayName);
+    const name = req.query.name; // 將 req.query.name 當作 HashMap 的 key
+    const timestamp = Date.now(); // 將目前時間轉換為時間戳
+
+    if (globalMap.has(name)) {
+        // 如果 name 已經存在於 HashMap 中，更新 timestamp
+        globalMap.get(name).timestamp = timestamp;
+        res.send(`使用者 ${name} 的 timestamp 已更新`);
     } else {
-        // 如果 session 不存在，創建一個並賦予 token
-        req.session.name = req.query.name;
-        req.session.displayName = req.query.displayName;
-        console.log("session create\n","name",req.session.name,"\n","displayName",req.session.displayName);
-        res.send('Token 已創建: displayName' + req.session.displayName);
+        // 如果 name 不存在於 HashMap 中，創建新的 entry
+        globalMap.set(name, {timestamp});
+        res.send(`使用者 ${name} 已存入`);
     }
 });
 
-setInterval(function() {
-    // 每 1 分鐘檢查 session 過期的用戶並向外部服務器發送通知
-    for(let sessionId in session.Session.sessions) {
-        let sessionObj = session.Session.sessions[sessionId];
-        if(Date.now() > sessionObj.cookie.expires) {
-            //刪除tunnel
-            console.log("刪除TUNEEL");
+setInterval(() => {
+    // 每 intervalTime 檢查 HashMap 中的每筆資料
+    const now = Date.now();
+
+    for (let [name, data] of globalMap) {
+        // 如果資料的 timestamp 加上過期時間小於目前時間，則刪除該筆資料
+        if (data.timestamp + expirationTime < now) {
+            globalMap.delete(name);
+            console.log(`資料 ${name} 已過期並被刪除`);
         }
     }
-}, 60000);
+}, intervalTime); // 每 intervalTime 執行一次檢查
 
-app.listen(port, () => {
-    console.log('Server started on port ',port);
+app.listen(3344, () => {
+    console.log('Server started on port 3344');
 });
